de0aa15fb18495a11f5c9a94dcb66c696bb14d71
gdindi021 <gdindi021@student.wethinkcode.co.za>
2022-10-20 09:12:35 +0200

    NUnit library project added from template

---
+++ ClassLibraryProjects/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace ClassLibraryProjects;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
